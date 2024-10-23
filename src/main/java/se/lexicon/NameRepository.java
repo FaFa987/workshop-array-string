17aa20c9c0396b87f1b5eb2210d7fc6ad160b278
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-23 19:32:01 +0200

    part1

--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -1,2 +1,31 @@
-package se.lexicon;public class NameRepository {
+package se.lexicon;
+
+import java.util.Arrays;
+
+public class NameRepository {
+
+    private static String[] names = new String[0];
+
+
+    public static int getSize() {
+        return names.length;
+
+    }
+
+    public static void setName(String[] names) {
+        NameRepository.names = names;
+        System.out.println(Arrays.toString(NameRepository.names));
+
+
+    }
+
+    public static void clear() {
+        NameRepository.names = new String[0];
+        System.out.println(Arrays.toString(NameRepository.names));
+    }
+
+    public static String[] findAll() {
+        return NameRepository.names;
+    }
 }
+
